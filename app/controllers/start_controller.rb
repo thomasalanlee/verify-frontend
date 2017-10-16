629b7bf78b6d4cc2cfd00af3905581b79b3b030c
Michael Walker <michael.walker@softwire.com>
2017-10-16 18:04:49 +0100

    TT-1394: Always record the Journey Type as a custom variable in Piwik.
    Authors: @rachelthecodesmith, @michaelwalker

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -9,7 +9,7 @@ class StartController < ApplicationController
       @tailored_text = current_transaction.tailored_text
       render :start_loa1
     else
-      render :start
+      render :start_loa2
     end
   end
 
@@ -17,10 +17,26 @@ class StartController < ApplicationController
   def request_post
     @form = StartForm.new(params['start_form'] || {})
     if @form.valid?
-      redirect_to @form.registration? ? about_path : sign_in_path
+      if @form.registration?
+        FEDERATION_REPORTER.report_registration(current_transaction, request)
+        redirect_to about_path
+      else
+        FEDERATION_REPORTER.report_sign_in(current_transaction, request)
+        redirect_to sign_in_path
+      end
     else
       flash.now[:errors] = @form.errors.full_messages.join(', ')
-      render :start
+      render :start_loa2
     end
+  end
+
+  def sign_in
+    FEDERATION_REPORTER.report_sign_in(current_transaction, request)
+    redirect_to sign_in_path
+  end
+
+  def register
+    FEDERATION_REPORTER.report_registration(current_transaction, request)
+    redirect_to choose_a_certified_company_path
   end
 end
